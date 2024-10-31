4014d5e6cac1a9bc0566cb0871168580f4348732
Your Name <wallicestenewaweru@gmail.com>
2024-10-31 13:48:56 +0300

    server URL

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,6 @@ const Navbar = () => {
   const [showNavMobile, setShowNavMobile] = useState(false);
   const [showNavbar, setShowNavbar] = useState(false);
   const [{ user }, dispatch] = useUserContext();
-  console.log(user);
   useEffect(() => {
     const navShow = () => {
       if (window.scrollY > 10) {
