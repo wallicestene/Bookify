041a5ec002cb106b68d64539b069e371f6ae0495
Kevin Muthami <muthamikevin80@gmail.com>
2025-02-09 13:59:44 +0300

    Commit

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,11 @@
 
 import PropertyContainer from "../components/PropertyContainer";
-
+import Carousel from '../components/carousel.jsx';
 const HomePage = () => {
   return (
     <section>
       <div>
+      <Carousel />
         <div>
           <PropertyContainer />
         </div>
