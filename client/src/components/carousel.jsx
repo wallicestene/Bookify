a7ac8bd7f08c9c857a22327c779f9228060b90e0
Kevin Muthami <muthamikevin80@gmail.com>
2025-02-11 21:03:28 +0300

    update

--- client/src/components/carousel.jsx
+++ client/src/components/carousel.jsx
@@ -1,6 +1,7 @@
 import { CCarousel, CCarouselCaption, CCarouselItem, CImage } from '@coreui/react';
 import React from 'react';
 
+import image from "../assets/loginFormImages/Bookify (200 x 200 px) (Website) (2).svg"
 const Carousel = () => {
   return (
     <CCarousel controls indicators dark>
@@ -12,7 +13,7 @@ const Carousel = () => {
         </CCarouselCaption>
       </CCarouselItem>
       <CCarouselItem>
-        <CImage className="d-block w-100" src="/images/vue.jpg" alt="slide 2" />
+        <CImage className="d-block w-100" src={image} alt="slide 2" />
         <CCarouselCaption className="d-none d-md-block">
           <h5>Second slide label</h5>
           <p>Some representative placeholder content for the second slide.</p>
