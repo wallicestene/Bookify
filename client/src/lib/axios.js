bd403541538595764de62f20ba08b8e295368bd2
wallicestene <wallicestenewaweru@gmail.com>
2025-11-09 17:20:42 +0300

    Handled Cors

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { toast } from "sonner";
 
+// Fallback to localhost:3000 if VITE_API_URL is not set
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 // Create axios instance with default config
